6c416cb584556c4f17e62e4586eb644c5a1a6523
Ivan Zotov <corvuss90@gmail.com>
2022-03-25 12:06:54 +0300

    feat: draft unpack

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
 
 func main() {
+	str := "a4bc2d5e"
 	fmt.Println("unpack string")
+	res := unpack(str)
+	fmt.Println(res)
+}
+
+func unpack(s string) string {
+
+	for i, r := range s {
+		if unicode.IsDigit(r) {
+			n, _ := strconv.Atoi(string(r))
+			prev := string(s[i-1])
+			fmt.Println(n, prev)
+		}
+
+	}
+
+	return s
 }
