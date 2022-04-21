4190edab05adeb3d6c7cf5c3bebdb5a0fe13c2b9
Ivan Zotov <corvuss90@gmail.com>
2022-04-21 23:52:31 +0300

    feat: handle escape backslash

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"unicode"
 )
 
 func main() {
-	str := `qwe\4\5`
+	str := `qwe\45`
 	res := unpack(str)
 	fmt.Printf("Given: %v, output: %v", str, res)
 }
@@ -27,27 +28,48 @@ func repeat(s string, n int) string {
 	return strings.Repeat(s, n-1)
 }
 
-func checkSymbol(r, prev, next rune) (code string, err error) {
+func savePickByIndex(str *string, i int) rune {
 
-	current := string(r)
+	if i < 0 || i > len((*str))-1 {
+		return 0
+	}
+
+	return rune((*str)[i])
+}
+
+func isEscaped(str *string, i int) bool {
+	prev := savePickByIndex(str, i-1)
+
+	if prev == 92 {
+		return true
+	}
+
+	return false
+}
+
+func checkSymbolNew(idx int, s *string) (code string, err error) {
+	current := savePickByIndex(s, idx)
+	prev := savePickByIndex(s, idx-1)
+	next := savePickByIndex(s, idx+1)
+
+	currentSymbol := string(current)
 	prevSymbol := string(prev)
-	// nextSymbol := string(next)
 
-	if current == "\\" && prevSymbol == "\\" {
+	if currentSymbol == "\\" && prevSymbol == "\\" {
 		code = "default"
 
 		return code, nil
 	}
 
-	if current == "\\" {
+	if currentSymbol == "\\" && !isEscaped(s, idx) {
 		code = "backslash"
 
 		return code, nil
 	}
 
-	if unicode.IsDigit(r) {
+	if unicode.IsDigit(current) {
 
-		if prevSymbol == "\\" {
+		if prevSymbol == "\\" && !isEscaped(s, idx-1) {
 			code = "default"
 
 			return code, nil
@@ -70,6 +92,50 @@ func checkSymbol(r, prev, next rune) (code string, err error) {
 
 	return code, nil
 }
+
+// func checkSymbol(r, prev, next rune) (code string, err error) {
+
+// 	current := string(r)
+// 	prevSymbol := string(prev)
+// 	// nextSymbol := string(next)
+
+// 	if current == "\\" && prevSymbol == "\\" {
+// 		code = "default"
+
+// 		return code, nil
+// 	}
+
+// 	if current == "\\" {
+// 		code = "backslash"
+
+// 		return code, nil
+// 	}
+
+// 	if unicode.IsDigit(r) {
+
+// 		if prevSymbol == "\\" {
+// 			code = "default"
+
+// 			return code, nil
+// 		}
+
+// 		if unicode.IsDigit(next) {
+// 			return code, errors.New("Wrong string")
+// 		}
+
+// 		if prev == 0 {
+// 			return code, errors.New("Wrong string")
+// 		}
+
+// 		return "num", nil
+// 	}
+
+// 	if prevSymbol == "\\" {
+// 		return code, errors.New("Wrong string")
+// 	}
+
+// 	return code, nil
+// }
 
 func processDefault(r rune, b *strings.Builder) {
 	b.WriteString(string(r))
@@ -101,22 +167,13 @@ func unpack(s string) string {
 
 	for i, r := range s {
 
-		prevIdx := i - 1
-		nextIdx := i + 1
+		var prev rune = savePickByIndex(&s, i-1)
 
-		var prev, next rune
-
-		if prevIdx >= 0 {
-			prev = rune(s[prevIdx])
-		}
-
-		if nextIdx <= len(s)-1 {
-			next = rune(s[nextIdx])
-		}
-
-		t, err := checkSymbol(r, prev, next)
+		t, err := checkSymbolNew(i, &s)
 
 		if err != nil {
+			log.Println(err.Error())
+
 			return ""
 		}
 
