164d4bc2c39fcbbd40fa121d773bda975d115760
Ivan Zotov <corvuss90@gmail.com>
2022-05-13 12:14:04 +0300

    refactor: code style fixes

--- pkg/unpackstring/parser.go
+++ pkg/unpackstring/parser.go
@@ -15,11 +15,7 @@ const CodeNumber = "num"
 func isEscaped(str *string, i int) bool {
 	prev := savePickByIndex(str, i-1)
 
-	if prev == BackslashRune {
-		return true
-	}
-
-	return false
+	return prev == BackslashRune
 }
 
 func checkSymbol(idx int, s *string) (code string, err error) {
@@ -51,18 +47,18 @@ func checkSymbol(idx int, s *string) (code string, err error) {
 		}
 
 		if unicode.IsDigit(next) {
-			return code, errors.New("Wrong string")
+			return code, errors.New("wrong string")
 		}
 
 		if prev == 0 {
-			return code, errors.New("Wrong string")
+			return code, errors.New("wrong string")
 		}
 
 		return CodeNumber, nil
 	}
 
 	if prevSymbol == BackslashChar {
-		return code, errors.New("Wrong string")
+		return code, errors.New("wrong string")
 	}
 
 	return code, nil
